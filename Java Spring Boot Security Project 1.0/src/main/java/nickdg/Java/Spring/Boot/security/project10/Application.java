14a98a10b96cbd4de40e7e79592e6bc5977f0f4e
Nick-DiGiacinto <151628711+Nick-DiGiacinto@users.noreply.github.com>
2024-04-19 12:32:37 +0200

    Project initialized

---
+++ Java Spring Boot Security Project 1.0/src/main/java/nickdg/Java/Spring/Boot/security/project10/Application.java
@@ -0,0 +1,13 @@
+package nickdg.Java.Spring.Boot.security.project10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Java Spring Boot Security Project 1.0/src/test/java/nickdg/Java/Spring/Boot/security/project10/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nickdg.Java.Spring.Boot.security.project10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
